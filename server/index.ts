a160c19767cc5da672ccaa995d43c38f0ff2faeb
gyuuuu <kai9702@gmail.com>
2021-01-15 11:05:46 +0900

    testtest

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('', (req, res, next) => {
-  res.json({ key : "value" })
+  res.json({ messgae : "value" })
 })
 
 app.listen(80, () => {
